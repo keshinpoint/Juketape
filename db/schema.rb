7dabac013323e7c4e97d03fb806e1e7f4df09afd
Keshav Vasudevan <kesavvasudevan@gmail.com>
2016-09-09 01:41:24 -0400

    Added error placeholder and remember me checkbox

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,59 @@ ActiveRecord::Schema.define(version: 20160908140053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "artists", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "full_name"
+    t.text     "description"
+    t.string   "website"
+    t.string   "address"
+    t.string   "username",               default: "", null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "coverpic_file_name"
+    t.string   "coverpic_content_type"
+    t.integer  "coverpic_file_size"
+    t.datetime "coverpic_updated_at"
+    t.index ["email"], name: "index_artists_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "facebook_networks", force: :cascade do |t|
+    t.string   "token",          default: ""
+    t.string   "secret",         default: ""
+    t.string   "page_token",     default: ""
+    t.integer  "artist_id",                   null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "selected_items", default: [],              array: true
+    t.string   "page_id",        default: ""
+  end
+
+  create_table "soundcloud_networks", force: :cascade do |t|
+    t.string   "access_token",   default: ""
+    t.string   "refresh_token",  default: ""
+    t.integer  "artist_id",                   null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "selected_items", default: [],              array: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                                          null: false
     t.string   "username",                                       null: false
@@ -29,6 +82,16 @@ ActiveRecord::Schema.define(version: 20160908140053) do
     t.string   "tag_line"
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
+  end
+
+  create_table "youtube_networks", force: :cascade do |t|
+    t.string   "access_token",   default: ""
+    t.string   "refresh_token",  default: ""
+    t.string   "expires_in",     default: ""
+    t.integer  "artist_id",                   null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "selected_items", default: [],              array: true
   end
 
 end
