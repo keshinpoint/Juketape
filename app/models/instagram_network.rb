799ef9ea8263d719f2e8141eb11d770e16e79c2b
Sagar Bommidi <sagar.bommidi@gmail.com>
2016-09-14 00:24:32 +0530

    Added images from Instagram in dashfolio

--- app/models/instagram_network.rb
+++ app/models/instagram_network.rb
@@ -1,5 +1,25 @@
+require 'instagram'
 class InstagramNetwork < ApplicationRecord
   belongs_to :user
   validates :access_token, presence: true
 
+  def load_instagram
+    @client ||= Instagram.new(access_token: access_token)
+  end
+
+  def user_info
+    load_instagram.get_user
+  end
+
+  def images
+    load_instagram.recent_images.map do |image|
+      {
+        link: image['link'],
+        url: image['images']['standard_resolution']['url'],
+        user_full_name: image['user']['full_name'],
+        user_id: image['user']['id'],
+        username: image['user']['username']
+      }
+    end
+  end
 end
--- lib/instagram.rb
+++ lib/instagram.rb
@@ -1,9 +1,10 @@
 class Instagram
-  attr_accessor :client_id, :client_secret, :redirect_uri
+  attr_accessor :client_id, :client_secret, :redirect_uri, :access_token
   def initialize(options={})
     @client_id = ENV['INSTAGRAM_CLIENT_ID']
     @client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
     @redirect_uri = options[:redirect_uri]
+    @access_token = options[:access_token]
   end
 
   def oauth_url(options)
@@ -28,6 +29,16 @@ class Instagram
     })
     result_body = JSON.parse(result.body)
     result_body['access_token']
+  end
+
+  def get_user
+    result = base_connection.get('/v1/users/self', { access_token: access_token })
+    JSON.parse(result.body)['data']
+  end
+
+  def recent_images
+    result = base_connection.get("/v1/users/#{get_user['id']}/media/recent", {access_token: access_token})
+    JSON.parse(result.body)['data']
   end
 
 
