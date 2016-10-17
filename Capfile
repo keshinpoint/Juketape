814973338fed4a77f6d542c0534acfea21da88ab
Sagar Bommidi <sagar.bommidi@gmail.com>
2016-10-17 22:40:29 +0530

    WIP: Setting up capistrano

--- Capfile
+++ Capfile
@@ -3,30 +3,10 @@ require "capistrano/setup"
 
 # Include default deployment tasks
 require "capistrano/deploy"
-
-# Include tasks from other gems included in your Gemfile
-#
-# For documentation on these, see for example:
-#
-#   https://github.com/capistrano/rvm
-#   https://github.com/capistrano/rbenv
-#   https://github.com/capistrano/chruby
-#   https://github.com/capistrano/bundler
-#   https://github.com/capistrano/rails
-#   https://github.com/capistrano/passenger
-#
-# require 'capistrano/rvm'
-# require 'capistrano/rbenv'
-# require 'capistrano/chruby'
-# require 'capistrano/bundler'
-# require 'capistrano/rails/assets'
-# require 'capistrano/rails/migrations'
-# require 'capistrano/passenger'
-
-require 'capistrano/bundler'
 require 'capistrano/rvm'
-require 'capistrano/rails/assets' # for asset handling add
-require 'capistrano/rails/migrations' # for running migrations
+require 'capistrano/bundler'
+require 'capistrano/rails/assets'
+require 'capistrano/rails/migrations'
 require 'capistrano/puma'
 
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,52 +1,30 @@
-# config valid only for current version of Capistrano
-lock '3.6.1'
-
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
 lock '3.6.1'
 
 set :application, 'juketape'
-set :repo_url, 'git@github.com:kesh92/juketape.git' # Edit this to match your repository
-set :branch, :master
-set :deploy_to, '/home/deploy/juketape'
+set :repo_url, 'git@github.com:kesh92/juketape.git'
+
+set :branch, :version2
+if ENV['BRANCH']
+  set :branch, ENV['BRANCH']
+end
+
+if ENV['DEPLOY_USER']
+  set :tmp_dir, "/tmp/portal-#{ENV['DEPLOY_USER']}"
+end
+
+set :scm, :git
+
+set :deploy_to, "/home/deploy/apps/juketape-#{fetch(:rails_env, 'production')}"
+
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
+set :use_sudo, false
+set :deploy_tag, Time.now.utc.strftime("%Y%m%d%H%M%S")
+
 set :rvm_type, :user
-set :rvm_ruby_version, 'ruby-2.3.1' # Edit this if you are using MRI Ruby
+set :rvm_ruby_version, 'ruby-2.3.0'
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
@@ -62,3 +40,26 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  after  :finishing, :restart
+end
