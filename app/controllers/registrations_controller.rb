3e5f2d6ab860f8da5cfd19c78c9ad32bb818f027
Keshav Vasudevan <kesavvasudevan@gmail.com>
2016-10-30 23:40:30 -0400

    updated in line edit styles

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,11 +15,11 @@ class RegistrationsController < Clearance::UsersController
             # Tell the UserMailer to send a welcome email after save
             UserMailer.welcome_email(@user).deliver_now
             
-            format.html { redirect_to(@user, notice: 'User was successfully created.') }
-            format.json { render json: @user, status: :created, location: @user }
-         else
-            format.html { render action: 'new' }
-            format.json { render json: @user.errors, status: :unprocessable_entity }
+            
+            
+         
+            
+            
          end
          
       end
